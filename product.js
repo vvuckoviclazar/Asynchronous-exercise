911d60b6f7471448eafa37618b4be6812d2a9e5e
Lazar Vuckovic <lazarvuckovic777@gmail.com>
2025-06-16 18:06:45 +0200

    Gallery click event added, back-btn added...

--- product.js
+++ product.js
@@ -25,7 +25,9 @@ const fetchProduct = async () => {
       <div class="product-detail-content">
        <div class="left">
        <button class="back-btn">Back to Products</button>
-        <img src="${images[0].url}" alt="${name}" class="main-image" />
+        <img src="${
+          images[0].url
+        }" alt="${name}" class="main-image" id="mainImage" />
         <div class="gallery">${imageGallery}</div>
        </div>
        <div class="right">
@@ -38,6 +40,29 @@ const fetchProduct = async () => {
        </div>
       </div>
     `;
+
+    const leftDiv = document.querySelector(".left");
+
+    leftDiv.addEventListener("click", (e) => {
+      const backBtn = e.target.closest(".back-btn");
+      const galleryImg = e.target.closest(".gallery-thumb");
+
+      if (backBtn) {
+        window.location.href = "index.html";
+        return;
+      }
+
+      if (galleryImg) {
+        const mainImage = document.getElementById("mainImage");
+        mainImage.src = galleryImg.src;
+        mainImage.alt = galleryImg.alt;
+        document
+          .querySelectorAll(".gallery-thumb")
+          .forEach((img) => img.classList.remove("selected"));
+
+        galleryImg.classList.add("selected");
+      }
+    });
   } catch (error) {
     console.error("Error loading product:", error);
   }
